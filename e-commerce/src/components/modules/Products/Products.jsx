a88a0de4c2a52fb8a28d9972a47cc46409db5845
Ayaan Ahmad <ayaanishere08@gmail.com>
2022-06-21 13:49:25 +0800

    Update Products.jsx

--- e-commerce/src/components/modules/Products/Products.jsx
+++ e-commerce/src/components/modules/Products/Products.jsx
@@ -15,63 +15,72 @@ data: [
     title: "The Dandy Chair",
     image: chair1,
     price: 200,
-    quantity: 1 
+    quantity: 1,
+    tags: "chair"
   },
   {
     id: 2,
     title: "The Hula Vase",
     image: vase1,
     price: 50,
-    quantity: 1 
+    quantity: 1,
+    tags: "vase"
   },
   {
     id: 3,
     title: "The Trinker Lamp",
     image: lamp1,
     price: 70,
-    quantity: 1 
+    quantity: 1,
+    tags: "lamp"
   },
   {
     id: 4,
     title: "Rustic Vase Set",
     image: vase2,
     price: 100,
-    quantity: 1 
+    quantity: 1,
+    tags: "vase"
   },
   {
     id: 5,
     title: "Grey Rust Sofa",
     image: sofa1,
     price: 450,
-    quantity: 1 
+    quantity: 1,
+    tags: "sofa"
   },
   {
     id: 6,
     title: "Birds Table",
     image: Fur1,
     price: 200,
-    quantity: 1 
+    quantity: 1,
+    tags: "table"
   },
   {
     id: 7,
     title: "Velvet Sofa",
     image: sofa2,
     price: 150,
-    quantity: 1 
+    quantity: 1,
+    tags: "sofa"
   },
   {
   id: 8,
   title: "Rust Furniture",
   image: Fur2,
   price: 200,
-  quantity: 1 
+  quantity: 1,
+  tags: "furniture"
   }, 
   {
     id: 9,
     title: "Valorant set",
     image: Fur3,
     price: 180,
-    quantity: 1 
+    quantity: 1,
+    tags: "furniture" 
   }    
 ]
 }
